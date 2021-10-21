b4b1a3d77db81bda02d7f2f3c1366eb824cbb09b
Denys Zheliezniak <deiskos@gmail.com>
2021-10-21 22:22:25 +0800

    rearranged ffmpeg arguments

--- ffmpeg_stream.py
+++ ffmpeg_stream.py
@@ -7,39 +7,31 @@ import time
 import settings
 import secrets
 
-process = None
-slider_position = 1
-
-def sigint_handler(sig, frame):
-    if process is not None:
-        process.kill()
-    sys.exit(0)
-signal.signal(signal.SIGINT, sigint_handler)
-
 def main():
     ffmpeg_cmd = (
         'ffmpeg',
         # image input
-        '-framerate 15 -re -loop 1 -i test_pattern.png -re',
+        '-re -framerate 30 -loop 1 -i test_pattern.png',
         # sound generation
-        '-f lavfi -i sine=frequency=1000',
+        '-re -f lavfi -i sine=frequency=1000:sample_rate=48000 -af volume=-18dB',
         # video processing and codecs
-        '-vf scale=1920:1080:flags=lanczos,setsar=1:1 -vcodec libx264 -preset ultrafast -tune stillimage',
+        '-vcodec libx264 -b:v 512k -preset ultrafast -tune stillimage',
         # audio codecs
-        '-b:v 128k -acodec aac -b:a 64k',
+        '-acodec aac -b:a 128k',
         # output format
         '-f flv -strict -2',
-        f'{settings.ingest}{secrets.stream_key}',
+        '{}{}'.format(settings.ingest, secrets.stream_key),
     )
 
-    process = Process(
-        target=subprocess.run,
-        args=(' '.join(ffmpeg_cmd).split(' '), ),
-        # kwargs={'stdout': subprocess.DEVNULL, 'stderr': subprocess.DEVNULL}
-    )
-    process.start()
+    process = subprocess.Popen(' '.join(ffmpeg_cmd).split(' '))
 
-    while process.is_alive():
+    def sigint_handler(sig, frame):
+        if process is not None:
+            process.kill()
+        sys.exit(0)
+    signal.signal(signal.SIGINT, sigint_handler)
+
+    while not process.poll():
         time.sleep(0.5)
 
 if __name__ == '__main__':
